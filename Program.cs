38305c3ef8ca7507e8b435f36227c54aed81151b
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-01-24 17:26:47 -0800

    added more integers, linked characters and strings

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace COMP003A.lectureactivity2
         {
             //Working with variables
             //the following code shows all three primary variable-related activities:
-            string username; //declaring a variable (1 0f 3)
+            string username; //declaring a variable (1 of 3)
             Console.WriteLine("What is your name?"); //request for user input
             username = Console.ReadLine(); //assigning a value to a variable (2 of 3)
             Console.WriteLine("Hi " + username); // retrieving its current value (3 of 3)
@@ -53,8 +53,80 @@ namespace COMP003A.lectureactivity2
             string exampleVARiable;
 
             Console.WriteLine("*********************************************");
+
+            /*types of variables and values matter in C# and are not interchangeable.
+             * there are eight integer types for storing integers of differing sizes and ranges:
+             * int, short, long, byte, sbyte, uint, ushort, and ulong. 
+             * the char type stores single characters.
+             * the string type stores longer text. 
+             * there are three types for storing real numbers: float, double, and decimal.
+             * the bool type stores truth values (true and false) used in logic.
+             * these types are the building blocks of a much larger type system.
+             * using var for a variable's type tells the compiler to infer its type from the surrounding code,
+             * so you do not have to type it out. (but it is still a specific type.)
+             * the System.Convert class is a useful class to convert from one type to another.
+             */
+
+            //declaring and using variables with integer types
+            byte aSingleByte = 34;
+            Console.WriteLine("aSingleByte: " + aSingleByte);
+            aSingleByte = 17;
+            Console.WriteLine("aSingleByte: " + aSingleByte);
+
+            short aNumber = 5039;
+            Console.WriteLine("aNumber: " + aNumber);
+            aNumber = -4354;
+            Console.WriteLine("aNumber: " + aNumber);
+
+            long aVeryBigNumber = 395904282569;
+            Console.WriteLine("aVeryBigNumber: " + aVeryBigNumber);
+            aVeryBigNumber = 13;
+            Console.WriteLine("aVeryBigNumber: " + aVeryBigNumber);
+
+            int anInteger = 2147483647;
+
+            //declaring, using, and concatenating (so linking things together in a chain) characters and strings 
+            char aLetter = 'A'; //character values are enclosed in single quotes
+            string message = "Hello World!"; //string values are enclosed in double quotes
+
+            Console.WriteLine("Concatenation using + :" + aLetter + " " + message);
+            //use the plus (+) sign in between characters/strings to concatenate (link)
+            Console.WriteLine($"Concatenation using string enterpolation : {aLetter} {message} ");
+            //use string interpolation with a dollar-sign ($) before the opening double quote and enclosing variables with curly braces ({variableName)}
+
+            //floating-point types
+            double number1 = 3.5623;
+            float number2 = 3.5623f;
+            decimal number3 = 3.5623m;
+            Console.WriteLine($"Value of number1: {number1}");
+            Console.WriteLine($"Value of number2: {number2}");
+            Console.WriteLine($"Value of number3: {number3}");
+
+            //scientific notation
+            double avogadrosNumber = 6.022e23;
+            Console.WriteLine($"Value of avogadrosNumber: {avogadrosNumber}");
+
+            //bool type
+            bool itWorked = true;
+            Console.WriteLine($"Value of itWorked: {itWorked}");
+            itWorked = false;
+            Console.WriteLine($"New value of itWorked: {itWorked}");
+
+            //convert
+            string inputAgeString;
+            int inputAge;
+            int currentYear = 2023;
+            Console.WriteLine($"Enter age in {currentYear}: ");
+            inputAgeString = Console.ReadLine();
+            inputAge = Convert.ToInt32(inputAgeString);
+            Console.WriteLine($"Your age is {inputAge}");
+
+            Console.WriteLine("************************************************************");
+
+
+
+
+
         }
-
-
     }
 }
