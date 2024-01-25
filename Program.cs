e525b6c1c50febe0be8892cddba0a6fbca7399db
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-01-24 16:18:58 -0800

    added variables

--- Program.cs
+++ Program.cs
@@ -10,7 +10,14 @@ namespace COMP003A.lectureactivity2
     {
         static void Main(string[] args)
         {
-           
+            //Working with variables
+            //the following code shows all three primary variable-related activities:
+            string username; //declaring a variable (1 0f 3)
+            Console.WriteLine("What is your name?"); //request for user input
+            username = Console.ReadLine(); //assigning a value to a variable (2 of 3)
+            Console.WriteLine("Hi" + username); // retrieving its current value (3 of 3)
         }
+
+
     }
 }
