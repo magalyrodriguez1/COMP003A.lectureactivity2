afc41b3facad4f022a14238885c0823f09969d06
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-01-24 16:00:03 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿/*Author: Magaly Rodriguez
+ * Course: COMP-003A
+ * Purpose: Lecture activity for variables, type system, math, and console properties
+ * Reference: The C# player's guide by RB Whitaker
+ * */
+
+namespace COMP003A.lectureactivity2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
