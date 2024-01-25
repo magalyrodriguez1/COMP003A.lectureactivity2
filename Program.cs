3cc755bf5819bd505e1b1c51ae07867c4f4c3a98
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-01-24 16:55:15 -0800

    added variable integers

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@
  * Reference: The C# player's guide by RB Whitaker
  * */
 
+using System.Runtime.InteropServices;
+
 namespace COMP003A.lectureactivity2
 {
     internal class Program
@@ -15,7 +17,42 @@ namespace COMP003A.lectureactivity2
             string username; //declaring a variable (1 0f 3)
             Console.WriteLine("What is your name?"); //request for user input
             username = Console.ReadLine(); //assigning a value to a variable (2 of 3)
-            Console.WriteLine("Hi" + username); // retrieving its current value (3 of 3)
+            Console.WriteLine("Hi " + username); // retrieving its current value (3 of 3)
+
+            //another example of the three primary variable related activities (declaring, assigning, and retrieving)
+            string favoriteColor;
+            Console.WriteLine("What is your favorite color?");
+            favoriteColor = Console.ReadLine();
+            Console.WriteLine("Your favorite color is " + favoriteColor);
+
+            //working with variable integers
+            int score; //declare a new variable with a data type int
+            //score = "generic user"; will fail to compile because the "0" is still assigned as a string - enclosed in the double quotes
+            score = 0; //works
+            score = 4; //you can reassign values to the variable as long as they are within scope and not constant
+            score = 11;
+            score = -1564;
+
+            //reading from a variable does not change it
+            int a;
+            int b;
+
+            a = 5;
+            b = 2;
+            Console.WriteLine("The current value of a: " + a);
+            Console.WriteLine("The current value of b: " + b);
+
+            b = a;
+            a = -3;
+            Console.WriteLine("The new value of a: " + a);
+            Console.WriteLine("The new value of b: " + b);
+
+            //variable are case sensitive. the variables below are different despite having similar names
+            string exampleVariable;
+            string exampleVAriable;
+            string exampleVARiable;
+
+            Console.WriteLine("*********************************************");
         }
 
 
